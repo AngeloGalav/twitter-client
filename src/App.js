dfe25f9e286693f1d4d08d73778393c0656698d4
AngeloGalav <tekken342@gmail.com>
2021-10-27 03:56:03 +0200

    aggiunto file temporaneo per interfacciarsi su twitter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-
+import TweetList from "./Components/TweetList";
 
 function App() {
   return (
     <div className="h-full">
       <div className="flex h-full gap-3 flex-col justify-center items-center">
       <h1 className="text-2xl">Hello Twitter!</h1>
+      <TweetList />
       <button
-      onClick={() => document.body.parentNode.getAttribute("data-theme") === "light" || !document.body.parentNode.getAttribute("data-theme")  ? document.body.parentNode.setAttribute("data-theme", "dark") : document.body.parentNode.setAttribute("data-theme", "light")} 
-      className="btn">Cambia tema!</button> 
+      onClick={() => document.body.parentNode.getAttribute("data-theme") === "light" || !document.body.parentNode.getAttribute("data-theme")  ? document.body.parentNode.setAttribute("data-theme", "dark") : document.body.parentNode.setAttribute("data-theme", "light")}
+      className="btn">Cambia tema!</button>
 
       </div>
-      
+
     </div>
   );
 }
