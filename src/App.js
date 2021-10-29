5d6001d5434bac2d1065e2f39f1ff7eb81e1ca03
Giacomo Ciccone <giacomo.ciccone2@studio.unibo.it>
2021-10-29 18:23:42 +0200

    Aggiunta mappa e reso componente il bottone temi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import TweetList from "./Components/TweetList";
+import SwitchTheme from "./Components/SwitchTheme";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 function App() {
   return (
@@ -6,9 +8,48 @@ function App() {
       <div className="flex h-full gap-3 flex-col justify-center items-center">
       <h1 className="text-2xl">Hello Twitter!</h1>
       <TweetList />
-      <button
-      onClick={() => document.body.parentNode.getAttribute("data-theme") === "light" || !document.body.parentNode.getAttribute("data-theme")  ? document.body.parentNode.setAttribute("data-theme", "dark") : document.body.parentNode.setAttribute("data-theme", "light")}
-      className="btn">Cambia tema!</button>
+      <SwitchTheme />
+      
+      <MapContainer
+      className="w-1/2 h-1/2"
+      center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
+      <TileLayer
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+        
+        <div className="w-72">
+        <img className="w-full h-full" src="https://i.ytimg.com/vi/Pg1ogBDAMuo/maxresdefault.jpg" />
+          </div>
+        
+        
+        </Popup>
+      </Marker>
+
+      <Marker position={[51.535, -0.0902]}>
+        <Popup>
+        
+        <div className="w-72">
+        <img className="w-full h-full" src="https://external-preview.redd.it/_-sfH6MwYEX1W8k_8wPS51_HihPvi88C6Pmjo8z3O1w.png?width=640&crop=smart&format=pjpg&auto=webp&s=16bf1e3851f69e19b963bf05add03ef32c6c3de5" />
+          </div>
+        
+        
+        </Popup>
+      </Marker>
+
+      <Marker position={[51.55, -0.0908]}>
+        <Popup>
+        
+        <div className="w-72">
+        <img className="w-full h-full" src="https://pbs.twimg.com/media/ElL0plwXUAMezy-.jpg" />
+          </div>
+        
+        
+        </Popup>
+      </Marker>
+    </MapContainer>
 
       </div>
 
---
+++ src/Components/SwitchTheme.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+
+const SwitchTheme = () => {
+    //TODO aggiungere stato per preservare il tema scelto dall'utente all'utima visita
+
+    //stato del tema attuale
+    const [theme, setTheme] = useState("light")
+
+    //inverte il tema
+    const changeTheme = () => {
+        if (theme === "light") {
+            setTheme("dark")
+        } else {
+            setTheme("light")
+        }
+    }
+
+    //funzione che viene eseguita ogni volta che viene cambiato il tema e aggiorna l'aspetto della pagina
+    useEffect(() => {
+        document.body.parentNode.setAttribute("data-theme", theme)
+    }, [theme])
+
+
+    return (
+        <div>
+            <button className="btn btn-ghost"
+            onClick={changeTheme}
+            >{theme === "light" ? <i className="bi bi-brightness-high-fill text-2xl"></i> : <i className="bi bi-moon text-xl"></i>}
+            </button>
+        </div>
+    )
+}
+
+export default SwitchTheme
