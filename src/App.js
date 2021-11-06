ee40da5182cd7ef67d9f78e92c0f3f6035654aa2
Giacomo Ciccone <giacomo.ciccone2@studio.unibo.it>
2021-11-06 20:48:57 +0100

    Implementata navbar per mobile e aggiunte funzionalita' minori

--- src/App.js
+++ src/App.js
@@ -1,132 +1,38 @@
 //Components
-import TweetList from "./Components/TweetList";
-import SearchBar from "./Components/SearchBar";
-import Tweet from "./Components/Tweet";
+import AboutScreen from "./Components/Screens/AboutScreen";
+import TweetsScreen from "./Components/Screens/TweetsScreen";
+import HomeScreen from "./Components/Screens/HomeScreen";
+import ScrollToTop from './Utils/ScrollOnTop';
 
 //router
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import SwitchTheme from "./Components/SwitchTheme";
-
 function App() {
-  const [primaRisposta, setRisposta] = useState(
-    <i>in attesa di risposta dal server...</i>
-  );
+    return (
+        //Il router di react permette di fare routing displayando i componenti in base all'url corrente
+        <Router>
+          {/* Riporta lo scroll sopra a tutto */}
+            <ScrollToTop />
 
-  function getData() {
-    axios
-      .get("/api")
-      .then((response) => {
-        setRisposta(response.data[0].trends[0].name);
-        console.log(primaRisposta);
-      })
-      .catch((error) => console.log(error.message));
-  }
+            {/* Lo switch restituisce il primo componente che fa match con l'url */}
+            <Switch>
+                
+                {/* exact fa in modo che l'url sia esattamente quello specificato in path, altrimenti restituisce il primo path incluso nell'url */}
+                <Route exact path="/" component={HomeScreen} />
 
-  useEffect(() => getData());
+                <Route exact path="/about" component={AboutScreen} />
 
-  return (
-    //Il router di react permette di fare routing displayando i componenti in base all'url corrente
-    <Router>
+                <Route exact path="/tweets" component={TweetsScreen} />
 
-      {/* Lo switch restituisce il primo componente che fa match con l'url */}
-      <Switch>
-
-        {/* exact fa in modo che l'url sia esattamente quello specificato in path, altrimenti restituisce il primo path incluso nell'url */}
-        <Route exact path="/" component={SwitchTheme} /> {/*Questa cosa andra' cambiata ovviamente, per il momento e' a scopo illustrativo, renderizza lo switch theme solo nella home (provate a mettere un url diverso)*/}
-
-        {/*Esiste anche questa altra sintassi per mostrare piu' di un componente ma si perde la props history in questo modo
+                {/*Esiste anche questa altra sintassi per mostrare piu' di un componente ma si perde la props history in questo modo
           <Route exact path="/product/:model">
             <Navbar static={true} />
             <ProductScreen />
           </Route>
         */}
-
-
-
-        
-      </Switch>
-
-      <button class="btn">neutral</button> 
-<button class="btn btn-primary">primary</button> 
-<button class="btn btn-secondary">secondary</button> 
-<button class="btn btn-accent">accent</button> 
-<button class="btn btn-ghost">ghost</button> 
-<button class="btn btn-link">link</button>
-<button class="btn btn-info">info</button> 
-<button class="btn btn-error">error</button> 
-<button class="btn btn-warning">warning</button> 
-<button class="btn btn-success">success</button> 
-<button class="btn btn-outline">neutral</button> 
-<button class="btn btn-outline btn-primary">primary</button> 
-<button class="btn btn-outline btn-secondary">secondary</button> 
-<button class="btn btn-outline btn-accent">accent</button>
-<button class="btn btn-outline btn-info">info</button> 
-<button class="btn btn-outline btn-error">error</button> 
-<button class="btn btn-outline btn-warning">warning</button> 
-<button class="btn btn-outline btn-success">success</button> 
-      <div className="h-full">
-          <div className="flex h-full gap-3 flex-col justify-center items-center">
-            <h1 className="text-2xl">Hello Twitter!</h1>
-            <TweetList />
-            <SearchBar />
-            <Tweet content={<b>{primaRisposta}</b>} />
-
-            <MapContainer
-              className="w-1/2 h-1/2"
-              center={[51.505, -0.09]}
-              zoom={13}
-              scrollWheelZoom={true}
-            >
-              <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={[51.505, -0.09]}>
-                <Popup>
-                  <div className="w-72">
-                    <img
-                      className="w-full h-full"
-                      alt="nice"
-                      src="https://i.ytimg.com/vi/Pg1ogBDAMuo/maxresdefault.jpg"
-                    />
-                  </div>
-                </Popup>
-              </Marker>
-
-              <Marker position={[51.535, -0.0902]}>
-                <Popup>
-                  <div className="w-72">
-                    <img
-                      className="w-full h-full"
-                      alt="nice"
-                      src="https://external-preview.redd.it/_-sfH6MwYEX1W8k_8wPS51_HihPvi88C6Pmjo8z3O1w.png?width=640&crop=smart&format=pjpg&auto=webp&s=16bf1e3851f69e19b963bf05add03ef32c6c3de5"
-                    />
-                  </div>
-                </Popup>
-              </Marker>
-
-              <Marker position={[51.55, -0.0908]}>
-                <Popup>
-                  <div className="w-72">
-                    <img
-                      className="w-full h-full"
-                      alt="nice"
-                      src="https://pbs.twimg.com/media/ElL0plwXUAMezy-.jpg"
-                    />
-                  </div>
-                </Popup>
-              </Marker>
-            </MapContainer>
-          </div>
-        </div>
-    </Router>
-
-
-  );
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,75 +1,107 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('daisyui'),
-  ],
-  daisyui: {
-    themes: [
-      {
-        'light': {
-          'primary' : '#1DA1F2',           /* Primary color */
-          'primary-focus' : '#0798EE',     /* Primary color - focused */
-          'primary-content' : '#ffffff',   /* Foreground content color to use on primary color */
+    purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+			inset: {
+				screen: "100vh", //top: 100vh;
+			}
+		},
+        screens: {
+            smartphone: "400px",
+            // => @media (min-width: 400px) { ... }
 
-          'secondary' : '#14171A',         /* Secondary color */
-          'secondary-focus' : '#0E1319',   /* Secondary color - focused */
-          'secondary-content' : '#ffffff', /* Foreground content color to use on secondary color */
+            tablet: "640px",
+            // => @media (min-width: 640px) { ... }
 
-          'accent' : '#e13960',            /* Accent color */
-          'accent-focus' : '#CF103D',      /* Accent color - focused */
-          'accent-content' : '#ffffff',    /* Foreground content color to use on accent color */
+            ipad: "768px",
+            // => @media (min-width: 768px) { ... }
 
-          'neutral' : '#3d4451',           /* Neutral color */
-          'neutral-focus' : '#2a2e37',     /* Neutral color - focused */
-          'neutral-content' : '#ffffff',   /* Foreground content color to use on neutral color */
+            laptop: "1024px",
+            // => @media (min-width: 1024px) { ... }
 
-          'base-100' : '#ffffff',          /* Base color of page, used for blank backgrounds */
-          'base-200' : '#f9fafb',          /* Base color, a little darker */
-          'base-300' : '#d1d5db',          /* Base color, even more darker */
-          'base-content' : '#1f2937',      /* Foreground content color to use on base color */
+            desktop: "1280px",
+            // => @media (min-width: 1280px) { ... }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: [
+            {
+                light: {
+                    primary: "#1DA1F2" /* Primary color */,
+                    "primary-focus": "#0798EE" /* Primary color - focused */,
+                    "primary-content":
+                        "#ffffff" /* Foreground content color to use on primary color */,
 
-          'info' : '#2094f3',              /* Info */
-          'success' : '#009485',           /* Success */
-          'warning' : '#ff9900',           /* Warning */
-          'error' : '#ff5724',             /* Error */
-        }
-      },
-      {
-        'dark': {
-          'primary' : '#1DA1F2',           /* Primary color */
-          'primary-focus' : '#0798EE',     /* Primary color - focused */
-          'primary-content' : '#ffffff',   /* Foreground content color to use on primary color */
+                    secondary: "#14171A" /* Secondary color */,
+                    "secondary-focus":
+                        "#0E1319" /* Secondary color - focused */,
+                    "secondary-content":
+                        "#ffffff" /* Foreground content color to use on secondary color */,
 
-          'secondary' : '#ffffff',         /* Secondary color */
-          'secondary-focus' : '#EFEFEF',   /* Secondary color - focused */
-          'secondary-content' : '#14171A', /* Foreground content color to use on secondary color */
+                    accent: "#e13960" /* Accent color */,
+                    "accent-focus": "#CF103D" /* Accent color - focused */,
+                    "accent-content":
+                        "#ffffff" /* Foreground content color to use on accent color */,
 
-          'accent' : '#e13960',            /* Accent color */
-          'accent-focus' : '#CF103D',      /* Accent color - focused */
-          'accent-content' : '#ffffff',    /* Foreground content color to use on accent color */
+                    neutral: "#3d4451" /* Neutral color */,
+                    "neutral-focus": "#2a2e37" /* Neutral color - focused */,
+                    "neutral-content":
+                        "#ffffff" /* Foreground content color to use on neutral color */,
 
-          'neutral' : '#16181d',           /* Neutral color */
-          'neutral-focus' : '#34373E',     /* Neutral color - focused */
-          'neutral-content' : '#ffffff',   /* Foreground content color to use on neutral color */
+                    "base-100":
+                        "#ffffff" /* Base color of page, used for blank backgrounds */,
+                    "base-200": "#f9fafb" /* Base color, a little darker */,
+                    "base-300": "#d1d5db" /* Base color, even more darker */,
+                    "base-content":
+                        "#1f2937" /* Foreground content color to use on base color */,
 
-          'base-100' : '#3d4451',          /* Base color of page, used for blank backgrounds */
-          'base-200' : '#222832',          /* Base color, a little darker */
-          'base-300' : '#162030',          /* Base color, even more darker */
-          'base-content' : '#ffffff',      /* Foreground content color to use on base color */
+                    info: "#2094f3" /* Info */,
+                    success: "#009485" /* Success */,
+                    warning: "#ff9900" /* Warning */,
+                    error: "#ff5724" /* Error */,
+                },
+            },
+            {
+                dark: {
+                    primary: "#1DA1F2" /* Primary color */,
+                    "primary-focus": "#0798EE" /* Primary color - focused */,
+                    "primary-content":
+                        "#ffffff" /* Foreground content color to use on primary color */,
 
-          'info' : '#2094f3',              /* Info */
-          'success' : '#009485',           /* Success */
-          'warning' : '#ff9900',           /* Warning */
-          'error' : '#ff5724',             /* Error */
-        }
-      },
-    ],
-  },
-}
+                    secondary: "#ffffff" /* Secondary color */,
+                    "secondary-focus":
+                        "#EFEFEF" /* Secondary color - focused */,
+                    "secondary-content":
+                        "#14171A" /* Foreground content color to use on secondary color */,
+
+                    accent: "#e13960" /* Accent color */,
+                    "accent-focus": "#CF103D" /* Accent color - focused */,
+                    "accent-content":
+                        "#ffffff" /* Foreground content color to use on accent color */,
+
+                    neutral: "#16181d" /* Neutral color */,
+                    "neutral-focus": "#34373E" /* Neutral color - focused */,
+                    "neutral-content":
+                        "#ffffff" /* Foreground content color to use on neutral color */,
+
+                    "base-100":
+                        "#3d4451" /* Base color of page, used for blank backgrounds */,
+                    "base-200": "#222832" /* Base color, a little darker */,
+                    "base-300": "#162030" /* Base color, even more darker */,
+                    "base-content":
+                        "#ffffff" /* Foreground content color to use on base color */,
+
+                    info: "#2094f3" /* Info */,
+                    success: "#009485" /* Success */,
+                    warning: "#ff9900" /* Warning */,
+                    error: "#ff5724" /* Error */,
+                },
+            },
+        ],
+    },
+};
