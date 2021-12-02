200e204bbc50c3c426a37ae988c1fa7d5b60cb4d
Matteo Baldazzi <matteo.baldazzi6@studio.unibo.it>
2021-12-02 16:42:03 +0100

    Light Bug Fix 'U'

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,7 +1,7 @@
 //qui vanno messi i reducers
 import { combineReducers } from "redux";
 import userReducer from "./userReducer";
-import tweetReducer from "./tweetReducer"
+import tweetReducer from "./tweetReducer";
 
 export default combineReducers({
     userReducer: userReducer,
