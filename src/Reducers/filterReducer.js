7d426bec9db599bdd4210f71872a66333f9a0055
Marco Amerotti <marco@aloy.lan>
2021-12-03 18:00:24 +0100

    pair programming

--- src/Reducers/filterReducer.js
+++ src/Reducers/filterReducer.js
@@ -1,42 +1,113 @@
 import { CHANGE_RADIUS, CHANGE_POSITIONS, CHANGE_NEW_SEARCH, CHANGE_DATE_RANGE, CHANGE_POPULAR, CHANGE_LANGUAGE, CHANGE_CENTER} from "../Actions/Types";
 
+
+
 const initalState = {
+
     radius : null, 
+
     position : null,
+
     newSearch : false, 
+
     dateRange : {startDate: new Date(new Date().setDate(new Date().getDate() - 7)),
+
     endDate: new Date()},
+
     popular : false,
+
     language : false,
+
     center : [41.9109, 12.4818]
+
 };
 
+
+
 export default function filterReducer(state = initalState, action) {
+
     switch (action.type) {
+
         case CHANGE_RADIUS:
+
             return {
+
                 ...state,
+
                 radius: action.payload.radius,
+
             };
+
         case CHANGE_POSITIONS:
+
             return {
+
                 ...state,
+
                 position: action.payload.position,
+
             };
+
         case CHANGE_NEW_SEARCH:
+
             return {
+
                 ...state,
+
                 newSearch: action.payload.newSearch,
+
             };
+
         case CHANGE_DATE_RANGE:
+
             return {
+
                 ...state,
+
                 dateRange: action.payload.dateRange,
+
             };
-//
+
+        case CHANGE_POPULAR:
+
+                    return {
+
+                        ...state,
+
+                        popular: action.payload.popular
+
+                    };
+
+        case CHANGE_LANGUAGE:
+
+                    return {
+
+                        ...state,
+
+                        language: action.payload.language,
+
+                    };
+
+        case CHANGE_CENTER:
+
+                    return {
+
+                        ...state,
+
+                        center: action.payload.center,
+
+                    };
+
         
+
         default:
+
             return state;
+
     }
+
 }
+
+
+
 
