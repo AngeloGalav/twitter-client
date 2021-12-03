b31a63add2bbc82eed656aa68e92f9755bc0f68b
AngeloGalav <tekken342@gmail.com>
2021-12-03 12:00:17 +0100

    added some sonar stuff

--- sonarscan.sh
+++ sonarscan.sh
@@ -1,5 +1,5 @@
 sonar-scanner \
   -Dsonar.projectKey=twitter-client_team11 \
-  -Dsonar.sources=. \
+  -Dsonar.sources= src, backend/middleware, backend/routes \
   -Dsonar.host.url=https://aminsep.disi.unibo.it/sonarqube \
   -Dsonar.login=f3b4a06fbebf6bb93f64df1fd5ae5a5ccd169ae6
