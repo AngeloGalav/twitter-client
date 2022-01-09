8a163ad8dd18e748cf45395201d9d35873f76d93
AngeloGalav <angelo.galavotti@studio.unibo.it>
2022-01-09 19:18:58 +0100

    refine deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 cd backend
-rm -rf build
-mv ../build .
+if [ -d "../build" ]; then
+    echo "Moving files from frontend build directory..."
+    # rm -rf build
+    # mv ../build .
+else
+    echo "frontend build directory not found..."
+fi
 rsync -av --exclude 'node_modules' . angelo.galavotti@annina.cs.unibo.it:/home/web/site202148/html/
