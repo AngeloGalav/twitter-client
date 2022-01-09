261491b06924d0607a1f0f952b4f3cf3f43bcbbd
AngeloGalav <angelo.galavotti@studio.unibo.it>
2022-01-09 18:46:10 +0100

    adjusted deploying script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-mv build backend/
 cd backend
-rsync -av --exclude 'node_modules' angelo.galavotti@annina.cs.unibo.it:/home/web/site202148/html/ .
-ssh angelo.galavotti@annina.cs.unibo.it 'cd /home/web/site202148/html/ && npm install && npm install twitter'
-ssh angelo.galavotti@annina.cs.unibo.it 'ssh gocker.cs.unibo.it "" '
-
+rm -rf build
+mv ../build .
+rsync -av --exclude 'node_modules' . angelo.galavotti@annina.cs.unibo.it:/home/web/site202148/html/
