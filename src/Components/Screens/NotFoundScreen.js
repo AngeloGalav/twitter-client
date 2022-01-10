1614fe7c0b75010a0ad0ffe3cccbec373a4984b7
Giacomo Ciccone <giacomo.ciccone2@studio.unibo.it>
2022-01-10 02:51:41 +0000

    Update NotFoundScreen.js

--- src/Components/Screens/NotFoundScreen.js
+++ src/Components/Screens/NotFoundScreen.js
@@ -9,26 +9,29 @@ const NotFoundScreen = () => {
     const [width, height] = useWindowSize();
 
     return (
-<div style={{minHeight: '1000px'}}  class="h-screen bg-gray-100 flex items-center">
-    {/* Navbar */}
+
+      <div style={{minHeight: '100vh'}} className="flex items-center py-10 ">
+      {/* Navbar */}
     <Navbar width={width} />
-	<div class="container flex gap-20 flex-col ipad:flex-row items-center justify-center px-5 text-gray-700">
-   		<div class="max-w-md">
-      		<div class="text-5xl font-dark font-bold">404</div>
-            <p
-              class="text-2xl ipad:text-3xl font-light leading-normal"
-            >Spiacenti, ciò che cercavi non si trova quì </p>
-          <p class="mb-8">Ma non ti preoccupare, ci sono molte altre cose che puoi fare con noi!</p>
-          
-          <Link to="/"><button class="px-4 inline py-2 text-sm font-medium leading-5 shadow btn btn-secondary">Torna alla home</button></Link>
-          
-    </div>
-      <div class="max-w-lg">
-      <img className="w-full h-full object-contain" src={notFoundSVG} alt="" />
-    </div>
-    
-  </div>
-</div>
+            <div className="container flex gap-20 flex-col ipad:flex-row items-center justify-center px-5">
+                   <div className="max-w-md">
+                      <div className="text-5xl font-dark font-bold">404</div>
+                    <p
+                      className="text-2xl ipad:text-3xl font-light leading-normal"
+                    >Spiacenti, ciò che cercavi non si trova quì </p>
+                  <p className="mb-8">Ma non ti preoccupare, ci sono molte altre cose che puoi trovare da noi!</p>
+                  
+                  <Link to="/"><button className="px-4 inline py-2 text-sm font-medium leading-5 shadow btn btn-secondary">
+                    <span className='text-secondary-content'>Torna alla home</span>  
+                    </button></Link>
+                  
+            </div>
+              <div className="max-w-lg">
+              <img className="w-full h-full object-contain" src={notFoundSVG} alt="" />
+            </div>
+            
+          </div>
+        </div>
     )
 }
 
