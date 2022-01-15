e7c144048f5ea9d257e74aba800888a7bf754231
AngeloGalav <angelo.galavotti@studio.unibo.it>
2022-01-15 20:27:23 +0100

    sonarqube files

--- sonarscan.sh
+++ sonarscan.sh
@@ -2,6 +2,7 @@
 
 sonar-scanner \
   -Dsonar.projectKey=team11-twitter-client \
-  -Dsonar.sources=src,backend/middleware,backend/routes \
+  -Dsonar.sources=src,backend/middleware,backend/routes,coverage \
   -Dsonar.host.url=https://aminsep.disi.unibo.it/sonarqube \
-  -Dsonar.login=bb4b2bff72d2f1eb8c5ee96d73e1ce893cd67734
+  -Dsonar.login=bb4b2bff72d2f1eb8c5ee96d73e1ce893cd67734 \
+  -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info 
