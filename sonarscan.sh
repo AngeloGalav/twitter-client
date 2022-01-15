0687cd5079be62fa347522cfdee11c9b681a581d
AngeloGalav <angelo.galavotti@studio.unibo.it>
2022-01-15 23:42:12 +0100

    trying to fix sonarqube

--- sonarscan.sh
+++ sonarscan.sh
@@ -2,7 +2,12 @@
 
 sonar-scanner \
   -Dsonar.projectKey=team11-twitter-client \
-  -Dsonar.sources=src,backend/middleware,backend/routes,coverage \
+  -Dsonar.sources=src,backend/middleware,backend/routes \
   -Dsonar.host.url=https://aminsep.disi.unibo.it/sonarqube \
   -Dsonar.login=bb4b2bff72d2f1eb8c5ee96d73e1ce893cd67734 \
-  -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info 
+  -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info \
+  -Dsonar.sourceEncoding=UTF-8 \
+  -Dsonar.tests=src/Components/__tests__/ \
+  -Dsonar.coverage.exclusions=**/__tests__/**,**/coverage/** \
+  -Dsonar.test.inclusions=**/__tests__/** \
+  -Dsonar.log.level=DEBUG
