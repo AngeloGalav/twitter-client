6d93293ad49dc2d665e07339cac3a430546d1881
AngeloGalav <angelo.galavotti@studio.unibo.it>
2022-01-17 12:32:22 +0100

    sonar properties.... odio sonarqube

--- sonarscan.sh
+++ sonarscan.sh
@@ -5,9 +5,11 @@ sonar-scanner \
   -Dsonar.sources=src,backend/middleware,backend/routes \
   -Dsonar.host.url=https://aminsep.disi.unibo.it/sonarqube \
   -Dsonar.login=bb4b2bff72d2f1eb8c5ee96d73e1ce893cd67734 \
-  -Dsonar.javascript.lcov.reportPaths=./coverage/lcov.info \
+  -Dsonar.language=js \
+  -Dsonar.tests=jest \
+  -Dsonar.javascript.lcov.reportPaths=coverage/lcov.info \
   -Dsonar.sourceEncoding=UTF-8 \
   -Dsonar.tests=src/Components/__tests__/ \
   -Dsonar.coverage.exclusions=**/__tests__/**,**/coverage/** \
   -Dsonar.test.inclusions=**/__tests__/** \
-  -Dsonar.log.level=DEBUG
+  # -Dsonar.log.level=DEBUG
